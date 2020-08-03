e894d85ab6dceb1ba72ee5252ccb516ded2a9e12
Jacob Daughtry <akutozo@gmail.com>
2020-08-02 20:29:01 -0500

    finishing up

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const inquirer = require('inquirer');
 
@@ -139,10 +139,18 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
